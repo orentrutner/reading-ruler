b15c911ddf9d62eb938c4a7019385668b537553c
Oren Trutner <orentrutner@gmail.com>
2019-05-29 18:29:49 -0700

    Prepare for watermark

--- main.js
+++ main.js
@@ -15,7 +15,16 @@
             opacity: 0.2;
 
             position: fixed;
-            z-index: 9999;
+            z-index: 2147483646;
+
+            pointer-events: none;
+            transition: all 0.1s;
+        }
+        .ruler-ff-extension-watermark {
+            border-bottom: 2px solid red;
+
+            position: fixed;
+            z-index: 2147483647;
 
             pointer-events: none;
             transition: all 0.1s;
@@ -27,17 +36,33 @@
     document.body.appendChild(ruler);
     ruler.className = 'ruler-ff-extension-ruler';
 
+    // create the watermark
+    const watermark = document.createElement('div');
+    document.body.appendChild(watermark);
+    watermark.className = 'ruler-ff-extension-watermark';
+
     // state
     const mousePosition = {
         x: 0,
         y: 0
     };
-    const elementRect = {
+    const hoverElementRect = {
         x: 0,
         y: 0,
         width: 0,
         height: 0
     };
+    let maxRulerPosition = 0;
+
+    function elementOfNode(node) {
+        for (let ancestor = node; ancestor; ancestor = ancestor.parentNode) {
+            if (ancestor.nodeType === 1) {
+                return ancestor;
+            }
+        }
+
+        return null;
+    }
 
     function caretInfoFromPoint(x, y) {
         if (document.caretPositionFromPoint) {
@@ -64,10 +89,17 @@
         }
 
         const rect = safeElement.getBoundingClientRect();
-        elementRect.x = rect.x;
-        elementRect.y = rect.y;
-        elementRect.width = rect.width;
-        elementRect.height = rect.height;
+        hoverElementRect.x = rect.x;
+        hoverElementRect.y = rect.y;
+        hoverElementRect.width = rect.width;
+        hoverElementRect.height = rect.height;
+    }
+
+    function positionElementAt(element, left, top, width, height) {
+        element.style.left = Math.round(left) + 'px';
+        element.style.top = Math.round(top) + 'px';
+        element.style.width = Math.round(width) + 'px';
+        element.style.height = Math.round(height) + 'px';
     }
 
     function positionRuler() {
@@ -76,23 +108,47 @@
             return;
         }
 
-        ruler.style.left = Math.round(elementRect.x - padding.x) + 'px';
-        ruler.style.width = Math.round(elementRect.width + 2 * padding.x) + 'px';
+        positionElementAt(
+            ruler,
+            hoverElementRect.x - padding.x,
+            caretInfo.rect.y - padding.y,
+            hoverElementRect.width + 2 * padding.x,
+            caretInfo.rect.height + 2 * padding.y);
 
-        ruler.style.top = Math.round(caretInfo.rect.y - padding.y) + 'px';
-        ruler.style.height = Math.round(caretInfo.rect.height + 2 * padding.y) + 'px';
+        maxRulerPosition = Math.max(maxRulerPosition, caretInfo.rect.y);
+        // positionElementAt(
+        //     watermark,
+        //     hoverElementRect.x,
+        //     maxRulerPosition,
+        //     hoverElementRect.width,
+        //     caretInfo.rect.height)
+
+        // const element = elementOfNode(caretInfo.node);
+        // if (element) {
+        //     element.style.opacity = Math.max(0.75, 0.95 * parseFloat(element.style.opacity) || 1);
+        // }
     }
 
+    const originalOnMouseMove = document.onmousemove;
     document.onmousemove = function(e) {
         mousePosition.x = e.x;
         mousePosition.y = e.y;
 
         trackHoverElement(e.target);
         positionRuler();
+
+        if (originalOnMouseMove) {
+            originalOnMouseMove(e);
+        }
     }
 
+    const originalOnScroll = document.onscroll;
     document.onscroll = function(e) {
         trackHoverElement(null);
         positionRuler();
+
+        if (originalOnScroll) {
+            originalOnScroll(e);
+        }
     }
 })();
